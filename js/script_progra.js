29a69260dee22fa0f79d690eed98d104f8d56088
eliWagn <110547027+eliWagn@users.noreply.github.com>
2022-12-16 10:03:31 +0100

    add-14

--- js/script_progra.js
+++ js/script_progra.js
@@ -11,9 +11,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
 
     btn_site[0].addEventListener('mouseover', () => {
-        if(getComputedStyle(site_web[0]).display != "none"){
-            site_web[0].style.display = "none";
-        }else{
+        if(getComputedStyle(site_web[0]).display != "grid"){
             site_web[0].style.display = "grid";
         }
         if(getComputedStyle(divers[0]).display != "none"){
@@ -21,9 +19,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }
     });
     btn_divers[0].addEventListener('mouseover', () => {
-        if(getComputedStyle(divers[0]).display != "none"){
-            divers[0].style.display = "none";
-        }else{
+        if(getComputedStyle(divers[0]).display != "grid"){
             divers[0].style.display = "grid";
         }
         if(getComputedStyle(site_web[0]).display != "none"){
@@ -32,9 +28,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
 
     btn_site[0].addEventListener('click', () => {
-        if(getComputedStyle(site_web[0]).display != "none"){
-            site_web[0].style.display = "none";
-        }else{
+        if(getComputedStyle(site_web[0]).display != "grid"){
             site_web[0].style.display = "grid";
         }
         if(getComputedStyle(divers[0]).display != "none"){
@@ -42,9 +36,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }
     });
     btn_divers[0].addEventListener('click', () => {
-        if(getComputedStyle(divers[0]).display != "none"){
-            divers[0].style.display = "none";
-        }else{
+        if(getComputedStyle(divers[0]).display != "grid"){
             divers[0].style.display = "grid";
         }
         if(getComputedStyle(site_web[0]).display != "none"){
@@ -52,52 +44,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }
     });
 
-    let svg = document.getElementsByClassName('colmar');
-    let txt_svg = document.getElementsByClassName('txt_svg');
-
-    txt_svg[0].style.display = "none";
-
-    svg[0].addEventListener('mouseover', () => {
-        if(getComputedStyle(txt_svg[0]).display != "grid"){
-            txt_svg[0].style.display = "grid";
-        }
-    });
-    svg[0].addEventListener('mouseout', () => {
-        if(getComputedStyle(txt_svg[0]).display != "none"){
-            txt_svg[0].style.display = "none";
-        }
-    });
-
-    let creepys = document.getElementsByClassName('creepypasta');
-    let txt_creepys = document.getElementsByClassName('txt_creepys');
-
-    txt_creepys[0].style.display = "none";
-
-    creepys[0].addEventListener('mouseover', () => {
-        if(getComputedStyle(txt_creepys[0]).display != "grid"){
-            txt_creepys[0].style.display = "grid";
-        }
-    });
-    creepys[0].addEventListener('mouseout', () => {
-        if(getComputedStyle(txt_creepys[0]).display != "none"){
-            txt_creepys[0].style.display = "none";
-        }
-    });
-
-    let portfolio = document.getElementsByClassName('portfolio');
-    let txt_portfolio = document.getElementsByClassName('txt_webGL');
-
-    txt_portfolio[0].style.display = "none";
-
-    portfolio[0].addEventListener('mouseover', () => {
-        if(getComputedStyle(txt_portfolio[0]).display != "grid"){
-            txt_portfolio[0].style.display = "grid";
-        }
-    });
-    portfolio[0].addEventListener('mouseout', () => {
-        if(getComputedStyle(txt_portfolio[0]).display != "none"){
-            txt_portfolio[0].style.display = "none";
-        }
-    });
+ 
 
 });
